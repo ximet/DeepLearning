dd673fab7922ef70cdde1fee37351105d28a7525
ximet <dq.ximet@gmail.com>
2017-04-03 18:10:51 +0300

    add function activation

--- python/perceptron.py
+++ python/perceptron.py
@@ -1,4 +1,9 @@
 from pylab import rand, plot, show, norm
+from math import exp
+
+
+def functionActivation(x):
+    return 1 / (1 + exp(-x))
 
 
 class Perceptron:
@@ -9,3 +14,7 @@ class Perceptron:
     def updateWeights(self, x, iterError):
         self.weight[0] += self.learningRate * iterError * x[0]
         self.weight[1] += self.learningRate * iterError * x[1]
+
+    def response(self, x):
+        y = x[0] * self.weight[0] + x[1] * self.weight[1]
+        return functionActivation(y)
