af3379329b27a10859aa97937be8cc84576693ba
ximet <dq.ximet@gmail.com>
2017-04-03 18:30:33 +0300

    add train and schema perceptron

--- python/perceptron.py
+++ python/perceptron.py
@@ -5,6 +5,20 @@ from math import exp
 def functionActivation(x):
     return 1 / (1 + exp(-x))
 
+#
+#                       input1
+#                                       F. activation
+#                      0000
+#                      0  0---------|   0000000
+#                      0000         |   0     0
+#                                   |   0     0   00000
+#                                   |---0  S  0---0 0 0
+#                       0000        |   0     0   00000
+#                       0  0--------|   0000000
+#                       0000                       output
+#
+#                    input2
+#
 
 class Perceptron:
     def __init__(self):
@@ -18,3 +32,19 @@ class Perceptron:
     def response(self, x):
         y = x[0] * self.weight[0] + x[1] * self.weight[1]
         return functionActivation(y)
+
+    def train(self, data):
+        learned = False
+        iteration = 0
+        while not learned:
+            globalError = 0.0
+            for desire in data:
+                actual = self.response(desire)
+                if desire[2] != actual:
+                    iterateError = desire[2] - actual
+                    self.updateWeights(desire, iterateError)
+                    globalError += abs(iterateError)
+            iteration += 1
+            if globalError == 0.0 or iteration >= 100:
+                print('iterations', iteration)
+                learned = True
